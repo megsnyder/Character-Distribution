fdc659fcb756c22b485129dfd71ee79ac76087a9
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-09-19 11:31:32 -0400

    Updated from Brython Server: 9/19/2018 11:31:32 AM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Meg
+Credit: None
 
 Assignment:
 
@@ -36,3 +36,4 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+text = input("Please enter a string of text (the bigger the better): ")
